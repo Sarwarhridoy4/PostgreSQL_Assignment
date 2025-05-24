beaf198d2db6405f055bc46a6bd1af863eda7453
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-05-24 21:18:37 +0600

    feat: solved problem 6

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -134,4 +134,10 @@ WHERE species_id NOT IN (
 );
 
 
-
+-- Problem 6: Show the most recent 2 sightings
+SELECT sp.common_name, s.sighting_time, r.name
+FROM sightings s
+JOIN species sp ON s.species_id = sp.species_id
+JOIN rangers r ON s.ranger_id = r.ranger_id
+ORDER BY s.sighting_time DESC
+LIMIT 2;
